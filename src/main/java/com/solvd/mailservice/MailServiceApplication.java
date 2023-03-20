f7d73cf924ba3c05b9ff6133d15730d425aced8e
ilisau <ilisov.laba@solvd.com>
2023-03-20 12:33:46 +0300

    Implement Kubernetes

--- src/main/java/com/solvd/mailservice/MailServiceApplication.java
+++ src/main/java/com/solvd/mailservice/MailServiceApplication.java
@@ -3,10 +3,10 @@ package com.solvd.mailservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @ConfigurationPropertiesScan
 public class MailServiceApplication {
 
